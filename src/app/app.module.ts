47c36496b289989e6ada9799b7996ceda89708ec
Margareth <margareth.aliendres@gmail.com>
2023-04-29 19:13:42 -0300

    first Commit"
    q
    wq

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomeComponent } from './pages/home/home.component';
+import { QuizzComponent } from './components/quizz/quizz.component';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizzComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    CommonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/quizz/quizz.component.ts
@@ -0,0 +1,89 @@
+import { Component, OnInit } from '@angular/core';
+import quizz__questions from '../../../assets/data/quizz__questions.json'
+
+@Component({
+  selector: 'app-quizz',
+  templateUrl: './quizz.component.html',
+  styleUrls: ['./quizz.component.css']
+})
+
+
+export class QuizzComponent implements OnInit{
+
+  title:string ="";
+
+  questions:any;
+  questionSelection:any;
+
+  answers:string[] = [];
+  answersSelected:string="";
+
+  questionIndex:number = 0;
+  questionMaxIndex:number=0;
+
+  finished:boolean = false;
+
+
+
+constructor(){
+
+
+}
+
+
+  ngOnInit(): void {
+ if(quizz__questions){
+     this.finished = false;
+     this.title = quizz__questions.title;
+
+     this.questions = quizz__questions.questions;
+     this.questionSelection = quizz__questions.questions[this.questionIndex];
+
+     this.questionIndex = 0;
+     this.questionMaxIndex = this.questions.length;
+
+     console.log( this.questionIndex);
+     console.log( this.questionMaxIndex
+      );
+
+
+ } }
+
+ playerChoose(value:string){
+  this.answers.push(value);
+  this.nexStep();
+
+
+ }
+
+ //a função vai executar assicrona//
+ async nexStep(){
+  this.questionIndex += 1;
+
+  if ( this.questionMaxIndex > this.questionIndex) {
+
+    this.questionSelection = this.questions[this.questionIndex]
+
+  }else{
+    const finalAnswer:string = await this.checkResult(this.answers)
+    this.finished = true;
+    this.answersSelected = quizz__questions.results[finalAnswer as keyof typeof quizz__questions.results]
+  }
+
+}
+  async checkResult(answers:string[]){
+  const result = answers.reduce((previous, current, i, arr)=> {
+if(
+    arr.filter(item => item === previous).length >
+    arr.filter(item => item === current).length
+){
+  return previous;
+
+}else{
+ return current;
+}
+  })
+
+  return result;
+  }
+}
